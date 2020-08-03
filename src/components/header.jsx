6600f11f47438236571dadc4ba2e5ab944b744e9
Ryan Monteiro <37312658+rtmonteiro@users.noreply.github.com>
2020-08-03 10:55:04 -0300

    Initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <div className="logo">
+      <span>REACT THREE FIBER</span>
+    </div>
+  </header>
+);
+
+export default Header;
